4e87e044f5c5cecfe50e0cdfe83e6e514a849595
nnachi_chima <chimakingsley216@gmail.com>
2022-09-02 05:49:05 -0700

    modified malloc

--- malloc.c
+++ malloc.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
 /**
- * _realloc - reallocates a pointer to double the space
+ * _realloc - function that reallocates a pointer to double the space
  * @ptr: pointer to the old array
  * @size: pointer to number of elements in the old array
- *
  * Return: pointer to the new array
  */
 char **_realloc(char **ptr, size_t *size)
